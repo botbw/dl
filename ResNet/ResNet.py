fa1864c9296064e8a32fc646a1b80125d8f3745a
botbw <wang1570@e.ntu.edu.sg>
2022-09-29 16:32:16 +0800

    finish resnet

--- ResNet/ResNet.py
+++ ResNet/ResNet.py
@@ -1,6 +1,7 @@
 from tensorflow.keras import layers, Sequential
 
 class ResBlk(layers.Layer):
+    expansion = 1
     def __init__(self, out_chan, strides = 1, downsample = None, **kwargs):
         super().__init__()
         
@@ -38,6 +39,7 @@ class ResBlk(layers.Layer):
         return y
 
 class BottleneckBlk(layers.Layer):
+    expansion = 4
     def __init__(self, out_chan, strides = 1, downsample = None, **kwargs):
         super().__init__()
    
@@ -45,11 +47,11 @@ class BottleneckBlk(layers.Layer):
         self.bn1 = layers.BatchNormalization()
         self.relu1 = layers.ReLU()
         
-        self.conv2 = layers.Conv2D(out_chan, padding='same', kernel_size=3, strides=strides)
+        self.conv2 = layers.Conv2D(out_chan, padding='same', kernel_size=3, strides=1)
         self.bn2 = layers.BatchNormalization()
         self.relu2 = layers.ReLU()
 
-        self.conv3 = layers.Conv2D(out_chan, padding='same', kernel_size=1, strides=strides)
+        self.conv3 = layers.Conv2D(out_chan * self.expansion, padding='same', kernel_size=1, strides=1)
         self.bn3 = layers.BatchNormalization()
 
         self.downsample = downsample
@@ -68,7 +70,7 @@ class BottleneckBlk(layers.Layer):
         y = self.relu2(y)
         
         y = self.conv3(y)
-        y = self.bm3(y, training)
+        y = self.bn3(y, training)
 
         h_x = X
         if self.downsample != None:
@@ -94,19 +96,19 @@ class ResNet(keras.Model):
     self.conv1 = conv1
 
     self.conv2_pool = layers.MaxPool2D(3, strides = 2, padding = "same")
-    self.conv2 = self.build_stage(blkType, True, 64, blk_num_list[0], strides = 1, name = 'conv2_x')
+    self.conv2_x = self.build_stage(blkType, True, 64, blk_num_list[0], strides = 1, name = 'conv2_x')
 
-    self.conv3 = self.build_stage(blkType, False, 128, blk_num_list[1], strides = 2, name = 'conv3_x')
+    self.conv3_x = self.build_stage(blkType, False, 128, blk_num_list[1], strides = 2, name = 'conv3_x')
 
-    self.conv4 = self.build_stage(blkType, False, 256, blk_num_list[2], strides = 2, name = 'conv4_x')
+    self.conv4_x = self.build_stage(blkType, False, 256, blk_num_list[2], strides = 2, name = 'conv4_x')
 
-    self.conv5 = self.build_stage(blkType, False, 512, blk_num_list[3], strides = 2, name = 'conv5_x')
+    self.conv5_x = self.build_stage(blkType, False, 512, blk_num_list[3], strides = 2, name = 'conv5_x')
   
   def build_stage(self, blkType, isFirstStage, out_chan, blk_num, strides = 1, name = None):
     downsample = None
-    if strides != 1 or isFirstStage == False:
+    if strides != 1 or isFirstStage == False or blkType == BottleneckBlk:
       downsample = keras.Sequential([
-          layers.Conv2D(out_chan, kernel_size=1, strides=strides),
+          layers.Conv2D(out_chan * blkType.expansion, kernel_size=1, strides=strides),
           layers.BatchNormalization()
       ])
 
@@ -121,10 +123,10 @@ class ResNet(keras.Model):
   def call(self, X, training = False, **kwargs):
     y = self.conv1(X)
     y = self.conv2_pool(y)
-    y = self.conv2(y)
-    y = self.conv3(y)
-    y = self.conv4(y)
-    y = self.conv5(y)
+    y = self.conv2_x(y)
+    y = self.conv3_x(y)
+    y = self.conv4_x(y)
+    y = self.conv5_x(y)
 
     return y
   
@@ -136,4 +138,19 @@ def resnet_34():
 def resnet_18():
   blkType = ResBlk
   blk_num_list = [2, 2, 2, 2]
+  return ResNet(blkType, blk_num_list)
+
+def resnet_50():
+  blkType = BottleneckBlk
+  blk_num_list = [3, 4, 6, 3]
+  return ResNet(blkType, blk_num_list)
+
+def resnet_101():
+  blkType = BottleneckBlk
+  blk_num_list = [3, 4, 23, 3]
+  return ResNet(blkType, blk_num_list)
+
+def resnet_152():
+  blkType = BottleneckBlk
+  blk_num_list = [3, 8, 36, 3]
   return ResNet(blkType, blk_num_list)
